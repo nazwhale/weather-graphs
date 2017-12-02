95e60ca28b60f07166f2d04adeb81115576e6c22
Naz Malik <naz@Nazs-MacBook-Pro.local>
2017-12-02 15:14:08 +0000

    Fetch data from openWeatherMap API

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '8b765b15d723ad23b66017a6625dc26a';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},gb`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
