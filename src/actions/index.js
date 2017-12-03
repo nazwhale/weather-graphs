42d944e7253b6835403cd9a84ac8cf1ba70ad773
Naz Malik <naz@Nazs-MacBook-Pro.local>
2017-12-03 15:03:44 +0000

    Connect weatherList reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,10 +7,18 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},gb`;
-  const request = axios.get(url);
+  const request = axios.get(url); // returns a promise - doesn't contain any data, just a request
 
   return {
     type: FETCH_WEATHER,
-    payload: request
+    payload: request  // returning promise as payload
   };
 }
+
+// redux-promise is a middleware
+// sees the incoming action, looks at the payload property
+// if payload is a promise, redux-promse stops the action
+// once request finishes, dispatches a new action of the same type
+// but: with a payload of the resolved request (response)
+// so: reducers get the data (and they can stay pure functions)
+// so: code seems synchronous!
